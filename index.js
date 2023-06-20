db8162a777674f817a8ebac660a66616fc76430f
Renz <crc1046@dlsud.edu.ph>
2023-06-20 17:23:45 +0800

    feat: setup db connection

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const taskRoutes = require('./routes/task');
+
+require("dotenv").config();
+const { MONGODB_USERNAME, MONGODB_PASSWORD } = process.env;
+
+const app = express();
+const PORT = 3000;
+
+// Port
+app.listen(PORT, () => {
+    console.log(`Your server is running on port ${PORT}`);
+}); 
+
+// DB Connection
+mongoose.connect(`mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@ecom-backend.kdyb994.mongodb.net/?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connection.on('error', console.error.bind(console, 'Connection Error'));
+mongoose.connection.once('open', () => console.log("Connected to DB"));
+
+// Middleware
+app.use(express.json());
+
+
+
+
