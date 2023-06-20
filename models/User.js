5d66f10434376e2f4b2cd05ac543882ccdc8925c
Renz <crc1046@dlsud.edu.ph>
2023-06-20 18:24:18 +0800

    refactor: changed user schema

--- models/User.js
+++ models/User.js
@@ -1,10 +1,6 @@
 var mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
-    username: {
-        type: String,
-        required: true
-    },
     email: {
         type: String,
         required: true
